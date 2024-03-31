de0b89878f487eb1db4c2a438ffb3292887bed50
Pratham <pratham3229@gmail.com>
2024-03-31 21:23:36 +0530

    Organizations

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,29 @@
-import { authMiddleware } from "@clerk/nextjs";
-
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 // See https://clerk.com/docs/references/nextjs/auth-middleware
 // for more information about configuring your Middleware
 export default authMiddleware({
   // Allow signed out users to access the specified routes:
   publicRoutes: ["/"],
+  afterAuth(auth, req, evt) {
+    if (auth.userId && auth.isPublicRoute) {
+      let path = "/select-org";
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      }
+      const orgSelection = new URL(path, req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+      const orgSelection = new URL("/select-org", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+  },
 });
 
 export const config = {
